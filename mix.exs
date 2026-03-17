c52fe9de0b0012eef92b0b6f27fb3c5250378153
Levi Buzolic <levibuzolic@gmail.com>
2026-03-17 14:36:41 +1100

    Bump version to 0.1.0-beta.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CinderUI.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/levibuzolic/cinder_ui"
-  @version "0.1.0-beta.1"
+  @version "0.1.0-beta.2"
 
   def project do
     [
